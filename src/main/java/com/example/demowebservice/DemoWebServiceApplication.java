263bb12273f9361882099db1d03c8bacda29f3e0
eduvzlacerda <eduardo.lacerda3@gmail.com>
2022-06-07 17:29:51 +0200

    test

--- src/main/java/com/example/demowebservice/DemoWebServiceApplication.java
+++ src/main/java/com/example/demowebservice/DemoWebServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWebServiceApplication {
 
     public static void main(String[] args) {
+        int o = 2;
         SpringApplication.run(DemoWebServiceApplication.class, args);
     }
 
