60bc1c221cc93d99ba5febf5e8e9764242b808ef
eduvzlacerda <eduardo.lacerda3@gmail.com>
2022-07-04 14:06:08 +0200

    add pagination to GET all users endpoint

--- src/main/java/com/example/demowebservice/controller/UserController.java
+++ src/main/java/com/example/demowebservice/controller/UserController.java
@@ -2,10 +2,10 @@ package com.example.demowebservice.controller;
 
 import com.example.demowebservice.model.DTO.UserDTO;
 import com.example.demowebservice.service.UserService;
+import com.example.demowebservice.utils.UserConverter;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,24 +23,32 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO){
         UserDTO createdUser = userService.createUser(userDTO);
-        return new ResponseEntity<>(userDTO,HttpStatus.CREATED);
+        return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
     }
-    @PutMapping()
-    public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UserDTO userDTO){
-        UserDTO updatedUser = userService.updateUser(userDTO);
-        return new ResponseEntity<>(userDTO,HttpStatus.OK);
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUserDetails( @PathVariable String id , @Valid @RequestBody UserDTO userDTO){
+        UserDTO updatedUser = userService.updateUserDetails(id,userDTO);
+        return new ResponseEntity<>(updatedUser,HttpStatus.OK);
 
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Class> deleteUser(@PathVariable String id){
+       userService.deleteUser(id);
+       return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserDetails(@PathVariable String id){
-       UserDTO userDTO = userService.findUserById(id);
+       UserDTO userDTO = UserConverter.convertUser2UserDTO(userService.findUserEntityById(id)) ;
         return new ResponseEntity<>(userDTO,HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDTO>> getUsers(){
-        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK) ;
+    public ResponseEntity<List<UserDTO>> getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                  @RequestParam(value = "limit", defaultValue = "25") int limit)
+    {
+        return new ResponseEntity<>(userService.getAllUsersPaginated(page ,limit),HttpStatus.OK) ;
     }
 
 }
--- src/main/java/com/example/demowebservice/service/UserService.java
+++ src/main/java/com/example/demowebservice/service/UserService.java
@@ -7,6 +7,9 @@ import com.example.demowebservice.model.entity.UserEntity;
 import com.example.demowebservice.repository.UserRepository;
 import com.example.demowebservice.utils.UserConverter;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,55 +31,64 @@ public class UserService implements UserDetailsService {
 
     public UserDTO createUser(UserDTO userDTO) {
 
-        if (userAlreadyExists(userDTO.getEmail())) {
+        if (userEmailAlreadyExists(userDTO.getEmail())) {
             throw new UserServiceException(ErrorMessages.RECORD_ALREADY_EXISTS.getErrorMessage());
         }
         setUserId(userDTO);
         encodePassword(userDTO);
-
         UserEntity savedUserEntity = userRepository.save(UserConverter.convertUserDTO2User(userDTO));
         return UserConverter.convertUser2UserDTO(savedUserEntity);
     }
 
-    public UserDTO updateUser(UserDTO userDTO) {
-        if(userDTO.getUserId() == null || !userRepository.existsById(userDTO.getUserId())){
-            throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
-        }
-        encodePassword(userDTO);
-        UserEntity updatedUserEntity = userRepository.save(UserConverter.convertUserDTO2User(userDTO));
+    public UserDTO updateUserDetails(String id,UserDTO userDTO) {
+
+        UserEntity userToUpdate = findUserEntityById(id);
+        userToUpdate.setFirstName(userDTO.getFirstName());
+        userToUpdate.setLastName(userDTO.getLastName());
+
+        UserEntity updatedUserEntity = userRepository.save(userToUpdate);
         return UserConverter.convertUser2UserDTO(updatedUserEntity);
     }
 
     public UserEntity findUserByEmail(String email) {
-        UserEntity userEntity = userRepository.findByEmail(email).orElseThrow(()->new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage()));
+        UserEntity userEntity = userRepository.findByEmail(email)
+                .orElseThrow(()->new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage()));
 
         return userEntity;
     }
 
-    public UserDTO findUserById(String id){
-       UserEntity userEntity = userRepository.findById(id).orElseThrow(()-> new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage()));
-        return UserConverter.convertUser2UserDTO(userEntity);
+    public UserEntity findUserEntityById(String id){
+       return userRepository.findById(id)
+               .orElseThrow(()-> new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage()));
+
     }
 
-    public List<UserDTO> getAllUsers() {
-        return userRepository.findAll().stream()
+    public List<UserDTO> getAllUsersPaginated(int page, int limit) {
+
+        Pageable paging = PageRequest.of(page,limit);
+
+        return userRepository.findAll(paging).stream()
                 .map( u -> UserConverter.convertUser2UserDTO(u))
                 .collect(Collectors.toList());
+    }
+    public void deleteUser(String id) {
+        UserEntity userEntity = findUserEntityById(id);
+        userRepository.delete(userEntity);
     }
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity userEntity = findUserByEmail(email);
         return new User(userEntity.getEmail(), userEntity.getEncPassword(), new ArrayList<>());
     }
-    private boolean userAlreadyExists(String email) {
-        return userRepository.findByEmail(email).get() != null;
+    private boolean userEmailAlreadyExists(String email) {
+        return userRepository.existsByEmail(email);
     }
     private void encodePassword(UserDTO userDTO) {
         userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
     }
-    private void setUserId(UserDTO userDTO) {
-        userDTO.setUserId(UUID.randomUUID().toString().replaceAll("_", ""));
-    }
 
+    private void setUserId(UserDTO userDTO) {
+        userDTO.setUserId(UUID.randomUUID().toString());
+    }
 
 }
